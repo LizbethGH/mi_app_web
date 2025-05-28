dd5b0bd6376e382c6c8e349317ed0259a249b794
LizbethGH <161343865+LizbethGH@users.noreply.github.com>
2025-05-28 16:35:43 -0600

    Update_ 2 main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://34.239.1.87:5000/api/devices";
+const API_URL = "http://3.236.164.214:5000/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
